a0c39b118572aa3a32181b62b9f3133b6b017e23
Gil Orcilla <mathnasiumcos@gmail.com>
2020-12-12 09:50:41 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// npm packages require to establish server connectivity
+const path = "path";
+const fs = require("fs");
+const express = require("express");
+
+// express set-up
+const app = express();
+
+// read/write notes elements
+
+// delete elements
+
+// api routing elements
+
+// html routing elements
