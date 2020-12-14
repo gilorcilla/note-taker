42dc60ce9fc34810ae4d51089f0da66a55291391
Gil Orcilla <mathnasiumcos@gmail.com>
2020-12-13 18:55:35 -0700

    adding lines 34-40

--- server.js
+++ server.js
@@ -20,11 +20,24 @@ var id;
 
 // read/write notes elements
 const readNotes = () => {
-  fs.readFile(__dirname + "/db/db.json", (err, res) => {
+  fs.readFile(__dirname + "/db/db.json", (err, response) => {
     notes = JSON.parse(response);
   });
 };
 
+const writeNotes = () => {
+  fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+  });
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 // delete elements
 
 // api routing elements
